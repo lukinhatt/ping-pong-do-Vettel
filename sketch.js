4af966b839b644b3171b8d4402e41cf6de4aa500
lukinhatt <marcelino.barros@escola.pr.gov.br>
2024-08-14 08:56:07 -0300

    funciona o jogo.js

---
+++ sketch.js
@@ -0,0 +1,95 @@
+function setup() {
+    createCanvas(1300, 1300);
+  }
+  
+  function draw() {
+    background("white");
+    fill ("black");
+    
+    textSize(69);
+    textAlign( CENTER, CENTER)
+    
+    if(mouseX < 50){
+      let palavra = "i";
+      text(palavra, CENTER, 300);
+  } else if(mouseX < 25) {
+      let palavra = "in"
+      text(palavra, 450, 300)
+  }
+    else if(mouseX < 50) {
+      let palavra = "inc"
+      text(palavra, 450, 300)
+  }else if(mouseX < 75) {
+      let palavra = "inco"
+      text(palavra, 450, 300)
+  }else if(mouseX < 100) {
+      let palavra = "incon"
+      text(palavra, 450, 300)
+  }else if(mouseX < 125) {
+      let palavra = "incons"
+      text(palavra, 450, 300)
+  }else if(mouseX < 150) {
+      let palavra = "inconst"
+      text(palavra, 450, 300)
+  }else if(mouseX < 175) {
+      let palavra = "inconsti"
+      text(palavra, 450, 300)
+  }else if(mouseX < 200) {
+      let palavra = "inconstitu"
+      text(palavra, 450, 300)
+  }else if(mouseX < 225) {
+      let palavra = "inconstituc"
+      text(palavra, 450, 300)
+  }else if(mouseX < 250) {
+      let palavra = "inconstituci"
+      text(palavra, 450, 300)
+  }else if(mouseX < 275) {
+      let palavra = "inconstitucio"
+      text(palavra, 450, 300)
+  }else if(mouseX < 300) {
+      let palavra = "inconstitucion"
+      text(palavra, 450, 300)
+  }else if(mouseX < 325) {
+      let palavra = "inconstituciona"
+      text(palavra, 450, 300)
+  }else if(mouseX < 350) {
+      let palavra = "inconstitucional"
+      text(palavra, 450, 300)
+  }else if(mouseX < 375) {
+      let palavra = "inconstitucionali"
+      text(palavra, 450, 300)
+  }else if(mouseX < 400) {
+      let palavra = "inconstitucionalis"
+      text(palavra, 450, 300)
+  }else if(mouseX < 425) {
+      let palavra = "inconstitucionaliss"
+      text(palavra, 450, 300)
+  }else if(mouseX < 450) {
+      let palavra = "inconstitucionalissi"
+      text(palavra, 450, 300)
+  }else if(mouseX < 475) {
+      let palavra = "inconstitucionalissim"
+      text(palavra, 450, 300)
+  }else if(mouseX < 500) {
+      let palavra = "inconstitucionalissima"
+      text(palavra, 450, 300)
+  }else if(mouseX < 525) {
+      let palavra = "inconstitucionalissimam"
+      text(palavra, 450, 300)
+  }else if(mouseX < 550) {
+      let palavra = "inconstitucionalissimame"
+      text(palavra, 450, 300)
+  }else if(mouseX < 575) {
+      let palavra = "inconstitucionalissimamen"
+      text(palavra, 450, 300)
+  }else if(mouseX < 600) {
+      let palavra = "inconstitucionalissimament"
+      text(palavra, 450, 300)
+  }else if(mouseX < 625, 300) {
+    let palavra = "Inconstitucionalissimamente"
+    text(palavra, 450, 300);
+  }
+    
+    
+  }
+    
